9f120e00d4ce96f36d560013033a78708dcde963
Ben Church <ben.douglas.church@gmail.com>
2020-03-30 12:40:59 -0600

    Initial commit

---
+++ lib/postgrex_pubsub.ex
@@ -0,0 +1,18 @@
+defmodule PostgrexPubsub do
+  @moduledoc """
+  Documentation for `PostgrexPubsub`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PostgrexPubsub.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/postgrex_pubsub_test.exs
@@ -0,0 +1,8 @@
+defmodule PostgrexPubsubTest do
+  use ExUnit.Case
+  doctest PostgrexPubsub
+
+  test "greets the world" do
+    assert PostgrexPubsub.hello() == :world
+  end
+end
